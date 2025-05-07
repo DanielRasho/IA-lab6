1616db307fbd31d7509088a4d5665ac2a01189bb
ElrohirGT <elrohirgt@gmail.com>
2025-05-07 15:17:14 -0600

    feat: Added color

--- console.go
+++ console.go
@@ -6,6 +6,45 @@ import (
 	sim "github.com/DanielRasho/IA-lab6/simulation"
 )
 
+type Color = int
+
+const (
+	BLACK Color = iota
+	RED
+	GREEN
+	YELLOW
+	BLUE
+	MAGENTA
+	CYAN
+	WHITE
+)
+
+type Style = int
+
+const (
+	Standard_Text       Style = 3
+	Standard_Background Style = 4
+	Bright_Text         Style = 9
+	Bright_Background   Style = 10
+)
+
+const RESET = "\x1b[0m"
+
+func S(content string, color Color, bgfg Style) string {
+	return fmt.Sprintf("\x1b[%d%dm%s%s", bgfg, color, content, RESET)
+}
+
+func StyleIfPlayer(mark sim.CellMark, i int64) string {
+	switch mark {
+	case sim.X:
+		return S(sim.CellToString(mark, i), GREEN, Standard_Text)
+	case sim.O:
+		return S(sim.CellToString(mark, i), BLUE, Standard_Text)
+	default:
+		return sim.CellToString(mark, i)
+	}
+}
+
 func ClearScreen() {
 	fmt.Print("\033[H\033[2J")
 }
@@ -14,9 +53,9 @@ func DisplayBoard(g sim.Game) {
 	for i := range 3 {
 		fmt.Printf(
 			"| %s | %s | %s |\n",
-			sim.CellToString(g.Board[i*3], int64(i*3+1)),
-			sim.CellToString(g.Board[i*3+1], int64(i*3+2)),
-			sim.CellToString(g.Board[i*3+2], int64(i*3+3)),
+			StyleIfPlayer(g.Board[i*3], int64(i*3+1)),
+			StyleIfPlayer(g.Board[i*3+1], int64(i*3+2)),
+			StyleIfPlayer(g.Board[i*3+2], int64(i*3+3)),
 		)
 	}
 }
