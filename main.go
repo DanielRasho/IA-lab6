00067cb43687034630695935380015b7dddcace6
Joaquin Puente <jpuente@paggoapp.com>
2025-05-07 20:36:30 -0600

    Monte Carlo implemented

--- main.go
+++ main.go
@@ -49,14 +49,72 @@ func FromString(s string) PlayerType {
 }
 
 type ProgramParams struct {
-	ShowBoard bool
-	Player1   PlayerType
-	Player2   PlayerType
+	ShowBoard      bool
+	Player1        PlayerType
+	Player2        PlayerType
+	RunExperiments bool
+}
+
+func RunExperiments() {
+	fmt.Println("Running Monte Carlo Tree Search experiments...")
+	
+	wins := 0
+	draws := 0
+	losses := 0
+	totalNodesExplored := 0
+	
+	mctsPlayer := players.NewMonteCarloPlayer()
+	minimaxPlayer := players.NewMiniMaxPlayer()
+	
+	N := 1000
+	for i := 0; i < N; i++ {
+		var game sim.Game
+		var mctsPlayerTurn sim.Turn
+		
+		if i%2 == 0 {
+			game = sim.NewGameWith(mctsPlayer, minimaxPlayer)
+			mctsPlayerTurn = sim.P1
+		} else {
+			game = sim.NewGameWith(minimaxPlayer, mctsPlayer)
+			mctsPlayerTurn = sim.P2
+		}
+		
+		game.Start()
+		
+		nodesExplored := 0
+		
+		for !game.ShouldEnd() {
+			if game.CurrentTurn == mctsPlayerTurn {
+				nodesExplored++
+			}
+			
+			game.Tick()
+		}
+		
+		report := game.Report()
+		if report.Winner == "No one" {
+			draws++
+		} else if (mctsPlayerTurn == sim.P1 && report.Winner == "Player 1") ||
+			(mctsPlayerTurn == sim.P2 && report.Winner == "Player 2") {
+			wins++
+		} else {
+			losses++
+		}
+		
+		totalNodesExplored += nodesExplored
+	}
+	
+	fmt.Printf("Results after %d experiments:\n", N)
+	fmt.Printf("Wins: %d (%.2f%%)\n", wins, float64(wins)/float64(N)*100)
+	fmt.Printf("Draws: %d (%.2f%%)\n", draws, float64(draws)/float64(N)*100)
+	fmt.Printf("Losses: %d (%.2f%%)\n", losses, float64(losses)/float64(N)*100)
+	fmt.Printf("Average nodes explored per game: %.2f\n", float64(totalNodesExplored)/float64(N))
 }
 
 func ParseProgramParams() ProgramParams {
 	params := ProgramParams{}
 	flag.BoolVar(&params.ShowBoard, "showBoard", true, "Describes whether or not the board should be displayed when making a game")
+	flag.BoolVar(&params.RunExperiments, "experiments", false, "Run MCTS experiments")
 
 	pTypeFormat := "The type of player %d. Valid values are:\n* h: Human\n* mi: Normal minimax\n* a: Minimax with alfa/beta prunning\n* mo: Montecarlo AI"
 
@@ -70,6 +128,7 @@ func ParseProgramParams() ProgramParams {
 	return params
 }
 
+
 func NewPlayerFromType(p PlayerType) sim.TicTacToePlayer {
 	switch p {
 	case HUMAN:
@@ -78,13 +137,21 @@ func NewPlayerFromType(p PlayerType) sim.TicTacToePlayer {
 		return players.NewMiniMaxPlayer()
 	case ALFA_BETA:
 		return players.NewAlfaBetaPlayer()
+	case MONTECARLO:
+		return players.NewMonteCarloPlayer()
 	default:
 		panic("Player type not implemented!")
 	}
 }
 
+
 func main() {
 	params := ParseProgramParams()
+
+	if params.RunExperiments {
+		RunExperiments()
+		return
+	}
 
 	fmt.Printf("Constructing game of %s againts %s...\n", PrettyDisplay(params.Player1), PrettyDisplay(params.Player2))
 	player1 := NewPlayerFromType(params.Player1)
